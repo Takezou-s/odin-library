3956ea3233a8645bab091b1c2e550dae2a18f3dc
Takezou0 <bkaya1228@gmail.com>
2023-07-18 01:56:30 +0300

    Finish app

--- app.js
+++ app.js
@@ -1,195 +1,7 @@
-const categoryItemTemplate = document.getElementById("category-item-template");
-const modalTemplate = document.getElementById("modal-template");
-const newBookFormTemplate = document.getElementById("new-book-form-template");
-const bookCardTemplate = document.getElementById("book-card-template");
-
 const addButton = document.getElementById("add-button");
 const bookContainer = document.querySelector(".book-container");
+const bookTypeContainer = document.getElementById("categories");
 
-const bookList = [];
-const bookTypes = [];
-
-let modalBackdropEl;
-let modalCardEl;
-let newBookFormEl;
-let activeBookType;
-
-class Modal {
-  constructor(backdrop, card) {
-    this.backdrop = backdrop;
-    this.card = card;
-    backdrop.addEventListener("click", this.hideModal.bind(this));
-  }
-
-  showModal(title, bodyContent) {
-    this.card.querySelector(".modal-card__header>h2").textContent = title;
-    this.card.querySelector(".modal-card__body").innerHTML = "";
-    this.card.querySelector(".modal-card__body").appendChild(bodyContent);
-    document.body.append(this.backdrop, this.card);
-  }
-
-  hideModal() {
-    if (this.backdrop) this.backdrop.remove();
-    if (this.card) this.card.remove();
-  }
-}
-
-class Form {
-  static _refNode;
-
-  static refNode(node) {
-    Form._refNode = node;
-  }
-  static createForm() {
-    return new Form(Form._refNode);
-  }
-
-  constructor(refNode) {
-    this.node = refNode.cloneNode(true);
-  }
-
-  get cancelButton() {
-    return this.node.querySelectorAll(".new-book-form__buttons>button")[1];
-  }
-
-  input(id) {
-    return this.node.querySelector(`#${id}`);
-  }
-}
-
-let modal, form;
-
-function init() {
-  modalBackdropEl = modalTemplate.content.querySelector(".modal-backdrop");
-  modalCardEl = modalTemplate.content.querySelector(".modal-card");
-  modal = new Modal(modalBackdropEl, modalCardEl);
-
-  newBookFormEl = newBookFormTemplate.content.querySelector("form");
-  Form.refNode(newBookFormEl);
-}
-
-function showNewBookForm(book) {
-  formHelper
-    .newNode()
-    .cancelButton()
-    .addEventListener("click", (event) => {
-      event.preventDefault();
-      closeModal();
-    });
-
-  formHelper.input("pageCount").addEventListener("change", (event) => {
-    formHelper.input("onPage").setAttribute("max", event.target.value);
-  });
-
-  if (book) {
-    for (const key in book) {
-      const el = formHelper.input(`${key}`);
-      if (el) {
-        el.value = book[key];
-      }
-    }
-  }
-
-  formHelper.presentNode().addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const formProps = Object.fromEntries(formData);
-
-    const newBook = new Book(
-      formProps.id === "null" ? Math.random().toString() : formProps.id,
-      formProps.title,
-      formProps.pageCount,
-      formProps.bookType,
-      formProps.description,
-      formProps.status,
-      formProps.author,
-      formProps.onPage
-    );
-    if (newBook.status === "Read") newBook.onPage = newBook.pageCount;
-
-    const existing = bookList.find((x) => x.id === newBook.id);
-    if (existing) {
-      for (const key in existing) {
-        if (Object.hasOwnProperty.call(newBook, key)) {
-          existing[key] = newBook[key];
-        }
-      }
-    } else {
-      bookList.push(newBook);
-    }
-    let bookType = bookTypes.find((x) => x === newBook.bookType);
-    if (!bookType) {
-      bookType = newBook.bookType;
-      bookTypes.push(bookType);
-      renderBookType(bookType);
-    }
-
-    if (activeBookType === bookType) {
-      renderBook(newBook);
-    } else {
-      const bookCard = document.querySelector(`[data-book-id='${book.id}']`);
-      if (bookCard) bookCard.remove();
-    }
-    closeModal();
-  });
-  showModal("New Book", formHelper.presentNode());
-}
-
-function renderBook(book) {
-  let bookCard = document.querySelector(`[data-book-id='${book.id}']`);
-  let existing = true;
-  if (!bookCard) {
-    bookCard = bookCardTemplate.content.cloneNode(true).querySelector(".book-card");
-    existing = false;
-  }
-
-  bookCard.style.borderLeft = `5px solid var(--${book.status.toLowerCase()}-color)`;
-  for (const key in book) {
-    const els = bookCard.querySelectorAll(`[data-for='${key}']`);
-    if (els && els.length > 0) {
-      for (const el of els) {
-        if (el) el.textContent = book[key];
-      }
-    }
-  }
-  bookCard.dataset.bookId = book.id;
-  if (!existing) {
-    document.querySelector(".book-container").insertAdjacentElement("afterbegin", bookCard);
-  }
-}
-
-function renderBookType(bookType) {
-  const liEl = categoryItemTemplate.content.cloneNode(true).querySelector("li");
-  const anchorEl = liEl.querySelector("a");
-  anchorEl.textContent = bookType;
-  anchorEl.addEventListener("click", (event) => {
-    event.preventDefault();
-    selectBookType(event.target.textContent);
-  });
-
-  document.getElementById("categories").append(liEl);
-}
-
-function selectBookType(bookType) {
-  activeBookType = bookType;
-  document.querySelector(".container>h1").textContent = bookType;
-  const bookArr = bookType ? bookList.filter((x) => x.bookType === bookType) : bookList;
-  document.querySelector(".book-container").innerHTML = "";
-  for (const book of bookArr) {
-    renderBook(book);
-  }
-  const elements = document.querySelectorAll(".category-item__link");
-  for (const element of elements) {
-    if (element.textContent !== bookType) {
-      element.parentElement.classList.remove("active");
-    } else {
-      element.parentElement.classList.add("active");
-    }
-  }
-}
-
-// Ctor function for Book object.
 function Book(id, title, pageCount, bookType, description, status, author, onPage) {
   this.id = id;
   this.title = title;
@@ -201,10 +13,325 @@ function Book(id, title, pageCount, bookType, description, status, author, onPag
   this.onPage = onPage;
 }
 
-init();
-addButton.addEventListener("click", showNewBookForm);
-bookContainer.addEventListener("click", (event) => {
-  if (event.target.dataset.bookId) {
-    showNewBookForm(bookList.find((x) => x.id === event.target.dataset.bookId));
+class Modal {
+  constructor(backdrop, card) {
+    this.backdrop = backdrop;
+    this.card = card;
+    backdrop.addEventListener("click", this.hideModal.bind(this));
   }
-});
+
+  showModal(title, bodyContent) {
+    this.placeContent(title, bodyContent);
+    document.body.append(this.backdrop, this.card);
+  }
+
+  placeContent(title, bodyContent) {
+    this.card.querySelector(".modal-card__header>h2").textContent = title;
+    this.card.querySelector(".modal-card__body").innerHTML = "";
+    this.card.querySelector(".modal-card__body").appendChild(bodyContent);
+  }
+
+  hideModal() {
+    if (this.backdrop) this.backdrop.remove();
+    if (this.card) this.card.remove();
+  }
+}
+
+class Form {
+  static getFormProps(form) {
+    const formData = new FormData(form.node);
+    return Object.fromEntries(formData);
+  }
+
+  constructor(refNode) {
+    this.node = refNode.cloneNode(true);
+  }
+
+  onSubmit(callbackFn) {
+    const submitHandler = ((event) => {
+      event.preventDefault();
+      callbackFn(event, Form.getFormProps(this));
+    }).bind(this);
+    this.node.onsubmit = submitHandler;
+  }
+
+  onReset(callbackFn) {
+    const resetHandler = ((event) => {
+      event.preventDefault();
+      if (this._controlledReset) {
+        this._controlledReset = false;
+      } else {
+        callbackFn(event);
+      }
+    }).bind(this);
+    this.node.onreset = resetHandler;
+  }
+
+  input(id) {
+    return this.node.querySelector(`#${id}`);
+  }
+
+  fillInputs(obj) {
+    this.clear();
+    if (obj) {
+      for (const key in obj) {
+        this.fillInput(key, obj[key]);
+      }
+    }
+  }
+
+  fillInput(id, value) {
+    this.input(id).value = value;
+  }
+
+  clear() {
+    this._controlledReset = true;
+    this.node.reset();
+  }
+}
+
+class BookManager {
+  constructor() {
+    this.bookList = [];
+    this.bookTypes = [];
+  }
+
+  addBook(book) {
+    book.id = Math.random().toString();
+    this.bookList.push(book);
+    this.addBookType(book.bookType);
+  }
+
+  updateBook(book) {
+    const existing = this.bookList.find((x) => x.id === book.id);
+    if (existing) {
+      for (const key in existing) {
+        if (Object.hasOwnProperty.call(book, key)) {
+          existing[key] = book[key];
+        }
+      }
+      this.addBookType(existing.bookType);
+    }
+  }
+
+  removeBook(book) {
+    const bookId = this.bookList.findIndex((x) => x.id === book.id);
+    if (bookId >= 0 && bookId < this.bookList.length) this.bookList.splice(bookId, 1);
+  }
+
+  addBookType(bookType) {
+    let existingBookType = this.bookTypes.find((x) => x === bookType);
+    if (!existingBookType) {
+      this.bookTypes.push(bookType);
+    }
+  }
+}
+
+class BookElementCreator {
+  constructor(bookTemplate, bookTypeTemplate) {
+    this.bookTemplate = bookTemplate;
+    this.bookTypeTemplate = bookTypeTemplate;
+  }
+
+  createOrUpdateBookElement(book) {
+    let bookCard = this.getBookElement(book);
+    let existing = true;
+    if (!bookCard) {
+      bookCard = this.bookTemplate.content.cloneNode(true).querySelector(".book-card");
+      existing = false;
+    }
+
+    bookCard.style.borderLeft = `5px solid var(--${book.status.toLowerCase()}-color)`;
+    for (const key in book) {
+      const els = bookCard.querySelectorAll(`[data-for='${key}']`);
+      if (els && els.length > 0) {
+        for (const el of els) {
+          if (el) el.textContent = book[key];
+        }
+      }
+    }
+    bookCard.dataset.bookId = book.id;
+    return { existing: existing, node: bookCard };
+  }
+
+  getBookElement(book) {
+    return document.querySelector(`[data-book-id='${book.id}']`);
+  }
+
+  removeBook(book) {
+    const el = this.getBookElement(book);
+    if (el) el.remove();
+  }
+
+  createBookTypeElement(bookType) {
+    const liEl = this.bookTypeTemplate.content.cloneNode(true).querySelector("li");
+    liEl.dataset.bookType = bookType;
+
+    const anchorEl = liEl.querySelector("a");
+    anchorEl.textContent = bookType;
+
+    return liEl;
+  }
+}
+
+class BookRenderer {
+  constructor(bookElementCreator, bookContainer, bookTypeContainer) {
+    this.bookElementCreator = bookElementCreator;
+    this.bookContainer = bookContainer;
+    this.bookTypeContainer = bookTypeContainer;
+  }
+
+  renderBook(clearParent, book) {
+    const result = this.bookElementCreator.createOrUpdateBookElement(book);
+    if (clearParent) {
+      this.bookContainer.innerHTML = "";
+    }
+    if (clearParent || !result.existing) {
+      this.bookContainer.insertAdjacentElement("afterbegin", result.node);
+    }
+  }
+
+  removeBook(book) {
+    this.bookElementCreator.removeBook(book);
+  }
+
+  renderBooks(clearParent, ...books) {
+    books.forEach((book) => this.renderBook(clearParent, book));
+  }
+
+  renderType(clearParent, bookType) {
+    if (clearParent) {
+      this.bookTypeContainer.innerHTML = "";
+    }
+    if (!this.bookTypeContainer.querySelector(`[data-book-type="${bookType}"]`) || clearParent) {
+      this.bookTypeContainer.insertAdjacentElement("afterbegin", this.bookElementCreator.createBookTypeElement(bookType));
+    }
+  }
+
+  renderTypes(clearParent, ...bookTypes) {
+    bookTypes.forEach((bookType) => this.renderType(clearParent, bookType));
+  }
+}
+
+class App {
+  static _instance;
+  static get() {
+    if (!this._instance) this._instance = new App();
+    return this._instance;
+  }
+
+  constructor(modal, form, bookManager, bookRenderer, showActiveBookType) {
+    this.modal = modal;
+    this.form = form;
+    this.bookManager = bookManager;
+    this.bookRenderer = bookRenderer;
+    this.showActiveBookType = showActiveBookType;
+    this.activeBookType = "";
+  }
+
+  createBook() {
+    this.form.onSubmit((event, formProps) => {
+      this._createOrUpdate(false, formProps);
+    });
+
+    this.form.clear();
+    this.modal.showModal("New Book", this.form.node);
+  }
+
+  updateBook(book) {
+    this.form.onSubmit((event, formProps) => {
+      this._createOrUpdate(true, formProps);
+    });
+    const bookObj = this.bookManager.bookList.find((x) => x.id === book) || book;
+    this.form.fillInputs(bookObj);
+    this.modal.showModal("Update: " + bookObj.title, this.form.node);
+  }
+
+  setActiveBookType(bookType) {
+    const changed = bookType !== this.activeBookType;
+    if (changed) {
+      this.activeBookType = bookType;
+      this.bookRenderer.renderBooks(true, ...this.bookManager.bookList);
+      this.showActiveBookType(bookType);
+    }
+  }
+
+  _createOrUpdate(update, formProps) {
+    const book = this._formPropsToBook(formProps, !update);
+    if (book.status === "Read") book.onPage = book.pageCount;
+    if (update) this.bookManager.updateBook(book);
+    else this.bookManager.addBook(book);
+    if (this.activeBookType === book.bookType) this.bookRenderer.renderBook(false, book);
+    else this.bookRenderer.removeBook(book);
+    this.bookRenderer.renderType(false, book.bookType);
+    this.modal.hideModal();
+  }
+
+  _formPropsToBook(formProps, generateId) {
+    const book = new Book(
+      formProps.id === generateId ? Math.random().toString() : formProps.id,
+      formProps.title,
+      formProps.pageCount,
+      formProps.bookType,
+      formProps.description,
+      formProps.status,
+      formProps.author,
+      formProps.onPage
+    );
+
+    return book;
+  }
+}
+
+function initModal() {
+  const template = document.getElementById("modal-template");
+  const modal = new Modal(template.content.querySelector(".modal-backdrop"), template.content.querySelector(".modal-card"));
+  return modal;
+}
+
+function initForm() {
+  const template = document.getElementById("new-book-form-template");
+  const form = new Form(template.content.querySelector("form"));
+  return form;
+}
+
+function initBookRenderer() {
+  const bookTypeTemplate = document.getElementById("category-item-template");
+  const bookCardTemplate = document.getElementById("book-card-template");
+  const bookRenderer = new BookRenderer(new BookElementCreator(bookCardTemplate, bookTypeTemplate), bookContainer, bookTypeContainer);
+  return bookRenderer;
+}
+
+function init() {
+  const modal = initModal();
+  const form = initForm();
+  const bookManager = new BookManager();
+  const bookRenderer = initBookRenderer();
+  const showActiveBookType = (bookType) => {
+    document.querySelector(".container>h1").textContent = bookType;
+    const elements = document.querySelectorAll(".category-item__link");
+    for (const element of elements) {
+      if (element.textContent !== bookType) {
+        element.parentElement.classList.remove("active");
+      } else {
+        element.parentElement.classList.add("active");
+      }
+    }
+  };
+
+  const app = new App(modal, form, bookManager, bookRenderer, showActiveBookType);
+
+  addButton.addEventListener("click", app.createBook.bind(app));
+  bookContainer.addEventListener("click", (event) => {
+    if (event.target.dataset.bookId) {
+      app.updateBook.call(app, event.target.dataset.bookId);
+    }
+  });
+  bookTypeContainer.addEventListener("click", (event) => {
+    if (event.target.parentElement.dataset.bookType) {
+      app.setActiveBookType.call(app, event.target.parentElement.dataset.bookType);
+    }
+  });
+}
+
+init();
